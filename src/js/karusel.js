36bbe2f8cb47825ffba56f1ecbb6522784f2ecf1
shket <shket.80@gmail.com>
2023-09-04 23:48:38 +0500

    MinSlide

--- src/js/karusel.js
+++ src/js/karusel.js
@@ -1,28 +1,32 @@
 
+//! Подсказки
+//minimg.insertAdjacentHTML('afterend', '<img src="img/karusel/img2.jpeg" alt="1">');
+//alert(slide.getAttribute('src'))
 
-const slider = document.querySelector('#slider');
+const slider = document.querySelector('#slider');//див со слайдами
 const sliderItem = Array.from(slider.children);// Превращаем HTML колекцию в массив
-let slideinfo = document.querySelector('.slideinfo');
-const next = document.querySelector('.next');
-const preev = document.querySelector('.preev');
-const minimg = document.querySelector('#minimg');
+let slideinfo = document.querySelector('.slideinfo');//Текс слайда
+const next = document.querySelector('.next');// Кнопка вперёд
+const preev = document.querySelector('.preev');// Кнопка назад
+const minimg = document.querySelector('#minimg');//<span id="minimg"></span> для создания минислайдов
+
 //Перебираем массив слайдов
 sliderItem.forEach(function (slide, index) {
-
-	//minimg.insertAdjacentHTML('afterend', '<img src="img/karusel/img2.jpeg" alt="1">');
-	//alert(slide.getAttribute('src'))
-	const srcslide = slide.getAttribute('src');
+	const srcslide = slide.getAttribute('src');//Инфо слайда
 	// Скрываем все слайды кроме первого
 	if (index !== 0) {
 		slide.classList.add('hiden');
-		minimg.insertAdjacentHTML('afterend',
+		// Создание HTML минислайда
+		minimg.insertAdjacentHTML('beforebegin',
 			`<img src="${srcslide}" data-index="${index}" class="minimage"  alt="1">`);
 	}
 	else {
-		minimg.insertAdjacentHTML('afterend',
-			`<img src="${srcslide}" data-index="${index}" data-activ class="minimage activimg"  alt="1">`);
+		// Создание HTML минислайда
+		minimg.insertAdjacentHTML('beforebegin',
+			`<img src="${srcslide}" data-index="${index}"  class="minimage activimg"  alt="1">`);
+
 		slideinfo.innerHTML = slide.getAttribute('src');
-		slide.setAttribute('data-activ', '');
+		slide.setAttribute('data-activ', '');// Добавляем атрибут
 	}
 	// Добавляем атрибут индекс
 	slide.dataset.index = index;
@@ -47,9 +51,12 @@ sliderItem.forEach(function (slide, index) {
 	// })
 })
 
+const minslide = document.querySelectorAll('.minimage');// Минислайд
+
+minimg.remove();// Удаляем временно созданный  HTML <span id="minimg"></span>
 next.addEventListener('click', function () {
-	//next.insertAdjacentHTML('beforebegin', '<img src="img/karusel/img3.jpeg" alt="3">');
 	const nextbtn = document.querySelector('[data-activ]');
+	const miniimg = document.querySelector('.activimg');
 	let nextSlideIndex;
 	const index = +nextbtn.dataset.index;
 	if (index + 1 === sliderItem.length) nextSlideIndex = 0;
@@ -60,8 +67,11 @@ next.addEventListener('click', function () {
 	// Скрываем текущий слайд
 	nextbtn.classList.add('hiden')
 	nextbtn.removeAttribute('data-activ');
+	miniimg.classList.remove('activimg');
 
 	// Показываем следующий слайд
+	let nextminimg = document.querySelector(`.minimage[data-index = "${nextSlideIndex}"]`);
+	nextminimg.classList.add('activimg');
 	nextSlide.classList.remove('hiden');
 	nextSlide.setAttribute('data-activ', '');
 	slideinfo.innerHTML = nextSlide.getAttribute('src');
@@ -69,6 +79,7 @@ next.addEventListener('click', function () {
 
 preev.addEventListener('click', function () {
 	const preevbtn = document.querySelector('[data-activ]');
+	const miniimg = document.querySelector('.activimg');
 	let nextSlideIndex;
 	const index = +preevbtn.dataset.index;
 	if (index - 1 < 0) nextSlideIndex = +sliderItem.length - 1;
@@ -79,9 +90,34 @@ preev.addEventListener('click', function () {
 	// Скрываем текущий слайд
 	preevbtn.classList.add('hiden')
 	preevbtn.removeAttribute('data-activ');
-
+	miniimg.classList.remove('activimg');
 	// Показываем следующий слайд
+	let nextminimg = document.querySelector(`.minimage[data-index = "${nextSlideIndex}"]`);
+	nextminimg.classList.add('activimg');
 	nextSlide.classList.remove('hiden');
 	nextSlide.setAttribute('data-activ', '');
 	slideinfo.innerHTML = nextSlide.getAttribute('src');
 })
+
+minslide.forEach(f => f.addEventListener('click', function () {
+	const miniimg = document.querySelector('.activimg');
+	const activSlide = document.querySelector('[data-activ]');
+	// Скрываем текущий слайд
+	activSlide.classList.add('hiden')
+	activSlide.removeAttribute('data-activ');
+	miniimg.classList.remove('activimg');
+
+	const minindex = +f.getAttribute('data-index');
+
+	let SlideIndex;
+	const index = +minindex;
+	// Находим следующий слайд
+	const slide = document.querySelector(`[data-index = "${index}"]`);
+
+	// Показываем следующий слайд
+
+	f.classList.add('activimg');
+	slide.classList.remove('hiden');
+	slide.setAttribute('data-activ', '');
+	slideinfo.innerHTML = slide.getAttribute('src');
+}));
